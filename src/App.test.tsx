9542322714ba9b55eb5323461f59e56d19e88ebf
David Ubanyi <ubanyi@gmail.com>
2021-07-01 14:43:33 +0100

    removed boiler plate cra and added testing for the app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders question component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/question 1/i);
+  expect(titleElement).toBeInTheDocument();
 });
