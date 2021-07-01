f307c1204fa62d2321a6e6357c668e794fdeec23
David Ubanyi <ubanyi@gmail.com>
2021-07-01 17:27:30 +0100

    added husky to handle linting and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { QuestionWrapper } from './components/questionWrapper/QuestionWrapper'
 
 function App() {
-
   return (
     <div style={{display: 'flex', justifyContent:'center'}}>
       <QuestionWrapper></QuestionWrapper>
